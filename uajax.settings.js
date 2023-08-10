128430738882b9ec423721af30dffd0619d98726
AucT <auct.eu@gmail.com>
2023-08-10 20:44:01 +0300

    add setting for jsonParseErrorSuffix

--- uajax.settings.js
+++ uajax.settings.js
@@ -5,6 +5,7 @@ var uajax = {
     notificationMessageObject: 'message', //on success if json response have message field it will use it instead of notificationMessageDefault
     notificationMessageHeader: 'uajax-note',
     htmlOnError: true, //if true and response is html, will insert into data-target html response even on error
+    jsonParseErrorSuffix: '<br>Error parsing JSON: Please contact webmaster', //if json can't be parse this will be added to response in message
 
 
     success: function (xhr, form, notificationMessage, displayNotification) {
